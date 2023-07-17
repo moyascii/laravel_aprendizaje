ae30899e3175083e1931c2c8f88bab81974704ac
Cristian Moya <cmoyacisternas@outlook.com>
2023-07-17 12:47:30 -0400

    Empezando eloqueeeeent

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -5,14 +5,11 @@
     <h1>Portfolio</h1>
 
     <ul>
-        {{--        @isset($portfolio)--}}
-        {{--            @if($portfolio)--}}
+
         @forelse ($portfolio as $portfolioItem)
             <li> {{ $portfolioItem['title'] }} </li>
         @empty
             <li>No hay proyectos para mostrar</li>
         @endforelse
-            {{--            @endif--}}
-            {{--        @endisset--}}
     </ul>
 @endsection
